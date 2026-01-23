1ca0575bbce3e84c9e110269877656a86181af1e
miquelburguera <miquelburguera@alumnat.iesdamiahuguet.net>
2026-01-23 10:45:46 +0100

    creuar barca

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const costat1 = ["Lladre", "Policia", "Mare", "Pare", "Fill1", "Fill2", "Filla1", "Filla2"];
-const barca = [];
+const costat1 = ["Mare", "Pare", "Fill1", "Fill2", "Filla1"];
+const barca = ["Filla2", "Policia", "Lladre"];
 const costat2 = [];
 
 const contenidorC1 = document.getElementById("personatges-c1");
+const contenidorC2 = document.getElementById("personatges-c2");
+const barco = document.getElementById("barca-display");
+
 const missatgeDisplay = document.getElementById("missatge");
 
 function actualitzarInterficie() {
@@ -18,13 +21,63 @@ function actualitzarInterficie() {
             let index = costat1.indexOf(element);
 
             // Mostram el missatge del que ha passat.  Recomanat només els errors
-            missatgeDisplay.textContent = `Has clicat: ${element}. Hauries de moure'l a la barca!`;
+            missatgeDisplay.textContent = `Has clicat: ${index} ${element}. Hauries de moure'l a la barca!`;
+
+            let creuarBarca = costat1.splice(index, 1);
+            barca.push(...creuarBarca)
+            console.log(costat1);
+            console.log(barca);
 
         });
 
         contenidorC1.appendChild(boto);
     });
+
+    barca.forEach(element => {
+        const boto = document.createElement("button");
+        boto.textContent = element;
+
+        boto.addEventListener("click", () => {
+            // Exemple de lògica: passar del costat 1 a la barca
+            let index = barca.indexOf(element);
+
+            // Mostram el missatge del que ha passat.  Recomanat només els errors
+            missatgeDisplay.textContent = `Has clicat: ${element}. Hauries de moure'l a la barca!`;
+            creuar()
+        });
+
+        barco.appendChild(boto);
+    });
+
+    costat2.forEach(element => {
+        const boto = document.createElement("button");
+        boto.textContent = element;
+
+        boto.addEventListener("click", () => {
+            // Exemple de lògica: passar del costat 1 a la barca
+            let index = costat2.indexOf(element);
+
+            // Mostram el missatge del que ha passat.  Recomanat només els errors
+            missatgeDisplay.textContent = `Has clicat: ${element}. Hauries de moure'l a la barca!`;
+
+        });
+
+        contenidorC2.appendChild(boto);
+    });
 }
+
+function creuar(){
+    let index = barca.indexOf(element);
+    let creuarcostat2 = barca.splice(index, 1);
+    console.log(barca);
+    console.log(costat2);
+    barca.push(...creuarcostat2)
+
+}
+
+// function repintar(){
+//     costat1.splice(1,0,  )
+// }
 
 // Inicialitzem la vista
 actualitzarInterficie();
